dbc2a1cba7a813faca41a2596eff9a46dbf7582c
rmill <rmiller003@gmail.com>
2021-03-28 17:44:45 -0400

    Initial commit

---
+++ tetrominos.py
@@ -0,0 +1,15 @@
+import pygame
+import random
+
+pygame.font.init()
+
+screen_width=790
+screen_height=690
+display_width=300
+display_height=590
+
+block_size=30
+
+x_topleft=(screen_width - display_width) //2
+y_topleft= screen_height - display_height
+
